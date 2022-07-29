1ed5706c8d64c9e95b39b40d626787742d993f38
Ayush Soni <mr.soni1002001@gmail.com>
2022-07-30 00:04:59 +0530

    fix: sidebar rendering and header changes

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export const Home: React.FunctionComponent = () => {
   return (
     <div className="home-screen-view">
-      <h1 className="leading-snug text-lg text-white font-semibold">Home is working</h1>
+      <h1 className="leading-snug text-lg text-white font-semibold">
+        Home is working
+      </h1>
     </div>
-  )
+  );
 };
